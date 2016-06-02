c71a96cd6eace1ccc4ed505bb22b1a08720ed450
Yoiner Gomez <Yoiner Gomez>
2016-06-01 19:56:48 -0500

    Ocultando la barra de navegacion

--- app/src/main/java/grp02/udea/edu/co/wakeupnow/AlarmaQR.java
+++ app/src/main/java/grp02/udea/edu/co/wakeupnow/AlarmaQR.java
@@ -3,10 +3,14 @@ package grp02.udea.edu.co.wakeupnow;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -19,12 +23,35 @@ public class AlarmaQR extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);*/
+
+        if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if(Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+
         setContentView(R.layout.activity_alarma_qr);
+
+
 
         activarAlarma();
         escanearCodigoQR();
     }
 
+    @Override
+    public void onBackPressed() {
+    }
 
     /**
      * Inicia el escaner del codigo QR
